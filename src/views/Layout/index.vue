349c9780a2e70216f7256cb5f49edad1ffaec32a
f_lesky <2451283865@qq.com>
2023-09-21 13:45:47 +0800

    layout static page

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    layout
-    <RouterView />
-  </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
+
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,3 +1,6 @@
 <template>
   <div>login</div>
 </template>
+
+<script setup>
+</script>
