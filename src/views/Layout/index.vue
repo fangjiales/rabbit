ab0db7844c3acabff783587dc4a85540570162b5
f_lesky <2451283865@qq.com>
2023-09-21 15:07:52 +0800

    layout 吸顶导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
@@ -9,4 +10,5 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 </script>
