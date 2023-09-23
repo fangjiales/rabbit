396ec74efa814c9ab6b61479c57be4e789950bb1
f_lesky <2451283865@qq.com>
2023-09-23 11:40:27 +0800

    二级分类-列表渲染

--- src/apis/category.js
+++ src/apis/category.js
@@ -8,3 +8,20 @@ export function getTopCategroyApi(id) {
     }
   })
 }
+
+export function getCategoryFilterApi(id) {
+  return request({
+    url: '/category/sub/filter',
+    params: {
+      id
+    }
+  })
+}
+
+export function getSubCategoryApi(data) {
+  return request({
+    url: '/category/goods/temporary',
+    method: 'POST',
+    data
+  })
+}
