c4e7c1e5bd806a7222f864f7a6df7e40ea149dfc
f_lesky <2451283865@qq.com>
2023-09-26 09:47:28 +0800

    shopping cart list render

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia"
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 
 export const useCartStore = defineStore(
@@ -9,7 +9,7 @@ export const useCartStore = defineStore(
     const addCart = (goods) => {
       const item = cartList.value.find((item) => item.skuId === goods.skuId)
       if (item) {
-        item.count++
+        item.count += goods.count
       } else {
         cartList.value.push(goods)
       }
@@ -17,8 +17,12 @@ export const useCartStore = defineStore(
     const delCart = (skuId) => {
       cartList.value = cartList.value.filter(item => item.skuId !== skuId)
     }
+    const allCount = computed(() => cartList.value.reduce((u, v) => u + v.count, 0))
+    const allPrice = computed(() => cartList.value.reduce((u, v) => u + (v.count * v.price), 0))
     return {
       cartList,
+      allCount,
+      allPrice,
       addCart,
       delCart
     }
