3d2a3c7d90d84ccc2db3f44a38e7e51d412f99bd
f_lesky <2451283865@qq.com>
2023-09-27 20:24:42 +0800

    pay

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -2,14 +2,17 @@
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { getOrderApi } from '@/apis/pay'
+import { useCountDown } from '@/composables/useCountDown'
 
 
 const route = useRoute()
 const payInfo = ref({})
+const { formatTime, start } = useCountDown()
 
 const getPayInfo = async () => {
   const res = await getOrderApi(route.query.id)
   payInfo.value = res.result
+  start(res.result.countdown)
 }
 
 onMounted(() => getPayInfo())
@@ -30,7 +33,7 @@ const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redire
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formatTime }}</span>, 超时后将取消订单</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
